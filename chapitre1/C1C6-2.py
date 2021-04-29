7763656bfec98b5efaeda38c46c27c5e466669db
Jessie Bakashika <jessie.bakashika@gmail.com>
2021-04-29 14:23:58 +0200

    Adding 4th and 5th player

--- chapitre1/C1C6-2.py
+++ chapitre1/C1C6-2.py
@@ -1,56 +1,56 @@
-firstPlayerName = input("Entrez le nom de la première personne :\n")
+firstPlayerName = input("Entrez le nom de la première personne :\t")
 firstPlayerCard = int(input(f"Quelle est le nombre de carte de {firstPlayerName} ? "))
-tempPlayernName = input("Entrez le nom de la deuxième personne: \n ")
-tempPlayerCard = int(input(f"Quel est le nombre carte de {tempPlayernName} ?")) 
+tempPlayerName = input("Entrez le nom de la deuxième personne: \t ")
+tempPlayerCard = int(input(f"Quel est le nombre carte de {tempPlayerName} ?")) 
 
 if firstPlayerCard > tempPlayerCard:	
-	secondPlayerName = tempPlayernName 
+	secondPlayerName = tempPlayerName 
 	secondPlayerCard = tempPlayerCard  
 else:  
-	secondPlayeName = firstPlayerName
-	firstPlayerName = tempPlayernName
+	secondPlayerName = firstPlayerName
+	firstPlayerName = tempPlayerName
 
 	secondPlayerCard = firstPlayerCard
 	firstPlayerCard = tempPlayerCard 
-
-tempPlayernName = input("Quel est le nom de la troisième personne: \n")
-tempPlayerCard = int(input(f"Quel est le nombre de carte {tempPlayernName} ?"))
+#3E Personne 
+tempPlayerName = input("Quel est le nom de la troisième personne: \t")
+tempPlayerCard = int(input(f"Quel est le nombre de carte {tempPlayerName} ?"))
 
 if secondPlayerCard > tempPlayerCard: 
-	thirdPlayerName = tempPlayernName
+	thirdPlayerName = tempPlayerName
 	thirdPlayerCard = tempPlayerCard
 
 elif tempPlayerCard > firstPlayerCard: 
 	thirdPlayerName = secondPlayerName
 	thirdPlayerCard = secondPlayerCard
 	
-	secondPlayeName = firstPlayerName
+	secondPlayerName = firstPlayerName
 	secondPlayerCard = firstPlayerCard
 
-	firstPlayerName = tempPlayernName
+	firstPlayerName = tempPlayerName
 	firstPlayerCard = tempPlayerCard 
 else: 
-	thirdPlayerName = secondPlayeName
+	thirdPlayerName = secondPlayerName
 	thirdPlayerCard = secondPlayerCard
 
-	secondPlayerName = tempPlayernName
+	secondPlayerName = tempPlayerName
 	secondPlayerCard = tempPlayerCard
-
-tempPlayernName = input("Quel est le nom de la troisième personne: \n")
-tempPlayerCard = int(input(f"Quel est le nombre de carte {tempPlayernName} ?"))
+#4E personne 
+tempPlayerName = input("Quel est le nom de la quatirème personne: \t")
+tempPlayerCard = int(input(f"Quel est le nombre de carte {tempPlayerName} ? \n"))
 
 if tempPlayerCard < thirdPlayerCard:
-	fourthPlayerName = tempPlayernName
+	fourthPlayerName = tempPlayerName
 	fourthPlayerCard = tempPlayerCard
 
 elif tempPlayerCard > firstPlayerCard:
 	fourthPlayerName = thirdPlayerName
 	fourthPlayerCard = thirdPlayerCard
 
-	thirdPlayerName = secondPlayeName
+	thirdPlayerName = secondPlayerName
 	thirdPlayerCard = secondPlayerCard
 
-	secondPlayeName = firstPlayerName
+	secondPlayerName = firstPlayerName
 	secondPlayerCard = firstPlayerCard
 
 	firstPlayerName = tempPlayerName
@@ -61,22 +61,79 @@ elif firstPlayerCard > tempPlayerCard > secondPlayerCard:
 	fourthPlayerName = thirdPlayerName
 	fourthPlayerCard = thirdPlayerCard
 
-	thirdPlayerName = secondPlayeName
+	thirdPlayerName = secondPlayerName
 	thirdPlayerCard = secondPlayerCard
 
-	secondPlayeName = tempPlayernName
+	secondPlayerName = tempPlayerName
 	secondPlayerCard = tempPlayerCard
 
 else: 
 	fourthPlayerName = thirdPlayerName
 	fourthPlayerCard = thirdPlayerCard
 
-	thirdPlayerName = tempPlayernName
+	thirdPlayerName = tempPlayerName
 	thirdPlayerCard = tempPlayerCard
+#5E personne
+tempPlayerName = input("Quel est le nom de la cinquième personne: \n")
+tempPlayerCard = int(input(f"Quel est le nombre de carte {tempPlayerName} ?"))
+
+if tempPlayerCard < fourthPlayerCard:
+	fifthPlayerName = tempPlayerName
+	fifthPlayerCard = tempPlayerCard
+
+elif tempPlayerCard > firstPlayerCard:
+	fifthPlayerName = fourthPlayerName
+	fifthPlayerCard = fourthPlayerCard 
+
+	fourthPlayerName = thirdPlayerName
+	fourthPlayerCard = thirdPlayerCard 
+
+	thirdPlayerName = secondPlayerName
+	thirdPlayerCard = secondPlayerCard
+
+	secondPlayerName = firstPlayerName
+	secondPlayerCard = firstPlayerCard
+
+	fifthPlayerName = tempPlayerName
+	firstPlayerCard = tempPlayerCard
+
+elif firstPlayerCard > tempPlayerCard > secondPlayerCard:
+	fifthPlayerName = fourthPlayerName
+	fifthPlayerCard = fourthPlayerCard 
+
+	fourthPlayerName = thirdPlayerName
+	fourthPlayerCard = thirdPlayerCard 
+
+	thirdPlayerName = secondPlayerName
+	thirdPlayerCard = secondPlayerCard
+
+	secondPlayerName = tempPlayerName
+	secondPlayerCard = tempPlayerCard
+elif secondPlayerCard > tempPlayerCard > thirdPlayerCard:
+
+	fifthPlayerName = fourthPlayerName
+	fifthPlayerCard = fourthPlayerCard 
+
+	fourthPlayerName = thirdPlayerName
+	fourthPlayerCard = thirdPlayerCard 
+
+	thirdPlayerName = tempPlayerName
+	thirdPlayerCard = tempPlayerCard
+
+else: 
+	fifthPlayerName = fourthPlayerName
+	fifthPlayerCard = fourthPlayerCard 
+
+	fourthPlayerName = tempPlayerName
+	fourthPlayerCard = tempPlayerCard	
+
+
+
 
 
 print(f"la première personne est {firstPlayerName} avec {firstPlayerCard} cartes Pokémon")
 print(f" La deuxième personne est {secondPlayerName} avec  {secondPlayerCard} cartes Pokémon")
 print(f"La troisième personne est {thirdPlayerName} avec {thirdPlayerCard} cartes Pokémon")
 print(f"La quatrième personne est {fourthPlayerName} avec  {fourthPlayerCard} cartes Pokémon")
+print(f"La cinquième personne est {fifthPlayerName} avec  {fifthPlayerCard} cartes Pokémon")
 
